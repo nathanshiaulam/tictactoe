2d308c94862d9d86238f85431d840b6ce76a8b55
jdcase <jdcase66@gmail.com>
2015-06-04 02:00:17 -0700

    main and prompt

--- tictactoe.java
+++ tictactoe.java
@@ -7,12 +7,15 @@ public class TicTacToe {
 
 	}
 
-	private int response(String board) {
-		return 1; 
+	private String response(String board) {
+		return ""; 
 	}
 
 	private void prompt(String board) {
-
+		StdOut.println(board.substring(0,3));
+		StdOut.println(board.substring(3,6));
+		StdOut.println(board.substring(6,9));
+		StdOut.println("Your move, bitch.");
 	}
 
 	private boolean gameOver(String board) {
@@ -23,6 +26,19 @@ public class TicTacToe {
 
 	}
 	public static void main(String[] argv) {
-
+		TicTacToe newGame = new TicTacToe();
+		String boardState = "000000000";
+		int index;
+		while (true) {
+			prompt(boardState);
+			index = StdIn.readInt();
+			String newState = boardState.substring(0,index) + '1' + 
+									boardState.substring(index + 1);
+			boardState = response(newState);
+			if (gameOver(boardState)) {
+				StdOut.println("Game Over, let's play again!");
+				boardState = "000000000";
+			}
+		}
 	}
 }
