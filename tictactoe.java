1d1f1aef818f4fd6d544a5447bddfb54622edb2b
nathanshiaulam <nlam@princeton.edu>
2015-06-04 01:02:34 -0700

    set up API of tictactoe

---
+++ tictactoe.java
@@ -0,0 +1,28 @@
+import java.util.*
+
+public class TicTacToe {
+
+
+	public TicTacToe() {
+		
+	}
+
+	private int response(String board) {
+		return 1; 
+	}
+
+	private void prompt(String board) {
+
+	}
+
+	private boolean gameOver(String board) {
+		return false;
+	}
+
+	private void legDay() {
+		
+	}
+	public static void main(String[] argv) {
+
+	}
+}
